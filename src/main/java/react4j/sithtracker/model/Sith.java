57cc9cadd81e7d5a1612c781cf227a4034951fc4
Peter Donald <peter@realityforge.org>
2018-12-19 23:34:51 +1100

    Add model for sith

---
+++ src/main/java/react4j/sithtracker/model/Sith.java
@@ -0,0 +1,39 @@
+package react4j.sithtracker.model;
+
+import arez.annotations.ArezComponent;
+import arez.annotations.Observable;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+@ArezComponent
+public abstract class Sith
+{
+  @Observable
+  @Nonnull
+  public abstract String getName();
+
+  public abstract void setName( @Nonnull String name );
+
+  @Observable
+  @Nonnull
+  public abstract World getHomeworld();
+
+  public abstract void setHomeworld( @Nonnull World homeworld );
+
+  @Observable
+  @Nullable
+  public abstract String getMaster();
+
+  public abstract void setMaster( @Nullable String master );
+
+  @Observable
+  @Nullable
+  public abstract String getApprentice();
+
+  public abstract void setApprentice( @Nullable String apprentice );
+
+  @Observable
+  public abstract boolean isLoaded();
+
+  public abstract void setLoaded( boolean loaded );
+}
