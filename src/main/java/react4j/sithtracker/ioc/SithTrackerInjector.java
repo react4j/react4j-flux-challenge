2918d1d03117c234ec026deef5a47c08c391971e
Peter Donald <peter@realityforge.org>
2020-03-12 13:21:38 +1100

    Use the generated factory rather than StingProvider related infrastructure

--- src/main/java/react4j/sithtracker/ioc/SithTrackerInjector.java
+++ src/main/java/react4j/sithtracker/ioc/SithTrackerInjector.java
@@ -3,11 +3,11 @@ package react4j.sithtracker.ioc;
 import javax.annotation.Nonnull;
 import react4j.sithtracker.model.SithTrackerModel;
 import react4j.sithtracker.views.SithListViewFactory;
-import react4j.sithtracker.views.SithTrackerView;
+import react4j.sithtracker.views.SithTrackerViewFactory;
 import react4j.sithtracker.views.SithViewFactory;
 import sting.Injector;
 
-@Injector( includes = { SithTrackerView.class,
+@Injector( includes = { SithTrackerViewFactory.class,
                         SithListViewFactory.class,
                         SithViewFactory.class,
                         SithTrackerModel.class } )
